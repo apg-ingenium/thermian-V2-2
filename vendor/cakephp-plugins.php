ece7c625d4d7040518f79acaf88ff117b2f6fc23
apg-ingenium <144331534+apg-ingenium@users.noreply.github.com>
2023-10-04 13:43:56 +0200

    Vendor upload 1
    Folders: bin, brick, dealerdirect, doctrine, jasny

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,13 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+
+return [
+    'plugins' => [
+        'Authentication' => $baseDir . '/vendor/cakephp/authentication/',
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'Cake/TwigView' => $baseDir . '/vendor/cakephp/twig-view/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'IdeHelper' => $baseDir . '/vendor/dereuromark/cakephp-ide-helper/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+    ],
+];
